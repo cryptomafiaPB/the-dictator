a1a68ba43dcf8ad0e51011288337ce525967033b
cryptomafiaPB <jugad360@gmail.com>
2025-01-15 17:51:58 +0530

    solve type errors while build

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -8,21 +8,21 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    // apiKey: process.env.FIREBASE_API_KEY,
-    // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    // projectId: process.env.FIREBASE_PROJECT_ID,
-    // storageBucket: process.env.FIREBASE_BUCKET_STORAGE,
-    // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    // appId: process.env.FIREBASE_APP_ID,
-    // measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.FIREBASE_API_KEY!,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
+    projectId: process.env.FIREBASE_PROJECT_ID!,
+    storageBucket: process.env.FIREBASE_BUCKET_STORAGE!,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
+    appId: process.env.FIREBASE_APP_ID!,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID!
 
-    apiKey: "AIzaSyAubvCCz6rmbvxZCZir8AZytImSdGJzd88",
-    authDomain: "thedictator-78cb1.firebaseapp.com",
-    projectId: "thedictator-78cb1",
-    storageBucket: "thedictator-78cb1.appspot.com",
-    messagingSenderId: "36069352041",
-    appId: "1:36069352041:web:53d5ef374e7e30e224f8f8",
-    measurementId: "G-WQ3EBCWDH1"
+    // apiKey: "AIzaSyAubvCCz6rmbvxZCZir8AZytImSdGJzd88",
+    // authDomain: "thedictator-78cb1.firebaseapp.com",
+    // projectId: "thedictator-78cb1",
+    // storageBucket: "thedictator-78cb1.appspot.com",
+    // messagingSenderId: "36069352041",
+    // appId: "1:36069352041:web:53d5ef374e7e30e224f8f8",
+    // measurementId: "G-WQ3EBCWDH1"
 
 };
 
